3267089bd6ecca1792bcf7465370a3137f75c64d
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2017-08-02 22:52:08 +0100

    Getting values through guzzle and api

--- app/Http/Controllers/CryptoController.php
+++ app/Http/Controllers/CryptoController.php
@@ -3,8 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
 
 class CryptoController extends Controller
 {
-    //
+    public function homepage() {
+    	$http_client = new Client();
+		$result = $http_client->get(env('API_VALUES_QUERY'))->getBody();
+		
+		$values = json_decode($result);
+    	
+		foreach ($values as $coin) {
+			switch ($coin->id) {
+				case 'bitcoin':
+					$btc_value_eur = $coin->price_eur;
+					$btc_hour_change = $coin->percent_change_1h;
+					$btc_day_change = $coin->percent_change_24h;
+					$btc_week_change = $coin->percent_change_7d;
+					break;
+				case 'ethereum':
+					$eth_value_eur = $coin->price_eur;
+					$eth_hour_change = $coin->percent_change_1h;
+					$eth_day_change = $coin->percent_change_24h;
+					$eth_week_change = $coin->percent_change_7d;
+					break;
+			}				
+		}
+
+    	return view('homepage')->with(compact('btc_value_eur', 'btc_hour_change', 'btc_day_change', 'btc_week_change'));
+    }
 }
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Crypto Values</title>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha256-rr9hHBQ43H7HSOmmNkxzQGazS/Khx+L8ZRHteEY1tQ4=" crossorigin="anonymous" />
+	</head>
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6">BTC</div>
+				<div class="col-md-6">ETH</div>
+			</div>
+		</div>
+	</body>
+</html>
