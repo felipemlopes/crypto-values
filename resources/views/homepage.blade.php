32ec3220d395a0a8f8ef3ccd2b295054bb393eba
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2017-08-10 23:37:46 +0100

    Scripts in bottom of page. Compressed images

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,12 +2,6 @@
 	<head>
 		<title>Homepage - Crypto Values</title>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-		<script>
-			var api_endpoint = '{!! env('API_VALUES_QUERY'); !!}';
-		</script>
-		<script src="{{ url('js/homepage.js') }}"></script>
-
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap-grid.min.css" />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/css/materialize.min.css" />
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous" />
@@ -59,4 +53,9 @@
 			</div>
 		</div>
 	</body>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+	<script>
+		var api_endpoint = '{!! env('API_VALUES_QUERY'); !!}';
+	</script>
+	<script src="{{ url('js/homepage.js') }}"></script>
 </html>
