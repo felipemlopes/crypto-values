1fc056fd830da1d62e9b2b833a44f1e1369d9d47
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2017-09-07 22:11:10 +0100

    Added links to charts

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,36 +4,40 @@
 <div class="container">
 	<div class="row valign-wrapper center-align values-container">
 		<div class="col-md-6">
-			<div class="card-panel hoverable">
-				<div class="currency-logo center-align">
-					<img src="{{ url('/images/btc-logo.png') }}" alt="bitcoin-logo">
+			<a href="/charts">
+				<div class="card-panel hoverable">
+					<div class="currency-logo center-align">
+						<img src="{{ url('/images/btc-logo.png') }}" alt="bitcoin-logo">
+					</div>
+					<h4 class="center-align">Bitcoin <small>(BTC)</small></h4>
+					<hr>
+					<h5 class="center-align" id="btc-actual-value"></h5>
+					<hr>
+					<div class="value-changes center-align">
+						<p><span id="btc-value-1h"></span> (last hour)</p>
+						<p><span id="btc-value-24h"></span> (last 24h)</p>
+						<p><span id="btc-value-7d"></span> (last week)</p>
+					</div>
 				</div>
-				<h4 class="center-align">Bitcoin <small>(BTC)</small></h4>
-				<hr>
-				<h5 class="center-align" id="btc-actual-value"></h5>
-				<hr>
-				<div class="value-changes center-align">
-					<p><span id="btc-value-1h"></span> (last hour)</p>
-					<p><span id="btc-value-24h"></span> (last 24h)</p>
-					<p><span id="btc-value-7d"></span> (last week)</p>
-				</div>
-			</div>
+			</a>
 		</div>
 		<div class="col-md-6">
-			<div class="card-panel hoverable">
-				<div class="currency-logo center-align">
-					<img src="{{ url('/images/eth-logo.png') }}" alt="ethereum-logo">
+			<a href="/charts">
+				<div class="card-panel hoverable">
+					<div class="currency-logo center-align">
+						<img src="{{ url('/images/eth-logo.png') }}" alt="ethereum-logo">
+					</div>
+					<h4 class="center-align">Ethereum <small>(ETH)</small></h4>
+					<hr>
+					<h5 class="center-align" id="eth-actual-value"></h5>
+					<hr>
+					<div class="value-changes center-align">
+						<p><span id="eth-value-1h"></span> (last hour)</p>
+						<p><span id="eth-value-24h"></span> (last 24h)</p>
+						<p><span id="eth-value-7d"></span> (last week)</p>
+					</div>
 				</div>
-				<h4 class="center-align">Ethereum <small>(ETH)</small></h4>
-				<hr>
-				<h5 class="center-align" id="eth-actual-value"></h5>
-				<hr>
-				<div class="value-changes center-align">
-					<p><span id="eth-value-1h"></span> (last hour)</p>
-					<p><span id="eth-value-24h"></span> (last 24h)</p>
-					<p><span id="eth-value-7d"></span> (last week)</p>
-				</div>
-			</div>
+			</a>
 		</div>
 	</div>
 </div>
