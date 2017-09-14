5374937f429a0bcb8a086bb6b696cdf39512169d
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2017-09-14 19:39:27 +0100

    Added sidebar menu

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,7 +13,44 @@
 	</head>
 	<body>
 		<div class="">
+
 			@yield('content')
+
+			<ul id="slide-out" class="side-nav">
+				<li><a class="subheader">Crypto Values</a></li>
+				<li><div class="divider"></div></li>
+				<li class="homepage-menu-link"><a href="/"><i class="material-icons">home</i> Homepage</a></li>
+				<li class="charts-menu-link"><a href="/charts"><i class="material-icons">show_chart</i> Trends charts</a></li>
+				<li class="calculator-menu-link"><a href="/calculator"><i class="material-icons">attach_money</i> Profit calculator</a></li>
+			</ul>
+
+			<div class="fixed-action-btn">
+				<a class="btn-floating btn-large deep-orange button-collapse pulse" data-activates="slide-out">
+					<i class="large material-icons">menu</i>
+				</a>
+			</div>
+
 		</div>
 	</body>
 </html>
+
+<script type="text/javascript">
+	 $(".button-collapse").sideNav();
+
+	 var url = window.location.pathname;
+
+	 switch (url) {
+	 	case '/':
+	 		$('.homepage-menu-link a').css('font-weight', 'bold');
+			$('.homepage-menu-link a').css('font-size', '16px');
+	 		break;
+		case '/charts':
+	 		$('.charts-menu-link a').css('font-weight', 'bold');
+			$('.charts-menu-link a').css('font-size', '16px');
+	 		break;
+		case '/calculator':
+			$('.calculator-menu-link a').css('font-weight', 'bold');
+			$('.calculator-menu-link a').css('font-size', '16px');
+			break;
+	 }
+</script>
